63c044f79851df2850f5222133685dbaef2efd17
Brandon Young <robotgenis@gmail.com>
2018-10-07 13:16:51 -0400

    v1.2.1
    Mini-auto for sampling
    Image proccessing for gold minerals

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/sampleTesting/config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/sampleTesting/config.java
@@ -3,7 +3,9 @@ package org.firstinspires.ftc.teamcode.roverRuckus.sampleTesting;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.PSConfigOpMode;
+import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.PSEnum;
 import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.PSRobot;
+import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.hardware.PSMotor;
 import org.firstinspires.ftc.teamcode.RobotLiveTest.RobotLiveDataTemp;
 
 import RobotLiveDataSending.RobotLiveData;
@@ -11,9 +13,18 @@ import RobotLiveDataSending.RobotLiveData;
 abstract class config extends PSConfigOpMode{
 
     RobotLiveDataTemp data;
+    PSMotor leftFront;
+    PSMotor rightFront;
+    PSMotor leftBack;
+    PSMotor rightBack;
 
     @Override
     public void config(OpMode opMode) {
         robot = new PSRobot(opMode);
+        leftFront = robot.motorHandler.newDriveMotor("LF", PSEnum.MotorLoc.LEFTFRONT,40);
+        rightFront = robot.motorHandler.newDriveMotor("RF", PSEnum.MotorLoc.RIGHTFRONT,40);
+        leftBack = robot.motorHandler.newDriveMotor("LB", PSEnum.MotorLoc.LEFTBACK,40);
+        rightBack = robot.motorHandler.newDriveMotor("RB", PSEnum.MotorLoc.RIGHTBACK,40);
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/sampleTesting/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/sampleTesting/test.java
@@ -6,13 +6,26 @@ import android.os.Environment;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.utils.vision.PSVisionUtils;
 import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.vision.UVC.UVCCamera;
 import org.firstinspires.ftc.teamcode.RobotLiveTest.RobotLiveSendTemp;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import RobotLiveDataSending.RobotLiveSend;
 
@@ -21,13 +34,27 @@ public class test extends config implements UVCCamera.Callback {
 
     UVCCamera camera;
     Bitmap bm;
+    int sampPos = 1; //0 = Left, 1 = center
+    int sampleNow;
+    double angle;
+    static String load = "";
+
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            load = "Error Loading!";
+        } else {
+            load = "Loaded Successfully!";
+        }
+    }
 
     @Override
     public void init() {
+        angle = Math.PI/2;
         config(this);
         camera = UVCCamera.getCamera(this);
 
-        data = RobotLiveSendTemp.createNewRun("http://192.168.0.116");
+        telemetry.addLine(load);
+        //data = RobotLiveSendTemp.createNewRun("http://192.168.0.116");
     }
 
     @Override
@@ -37,32 +64,100 @@ public class test extends config implements UVCCamera.Callback {
 
     @Override
     public void loop() {
-
-        if(data == null){
-            telemetry.addData("Error","data is null");
-
-        }else {
-            if (bm != null) {
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                bm.compress(Bitmap.CompressFormat.JPEG, 9 /*ignored for PNG*/, bos);
-                byte[] bitmapdata = bos.toByteArray();
-                ByteArrayInputStream bs = new ByteArrayInputStream(bitmapdata);
-
-
-                data.addLiveImage(bs);
+        if (gamepad1.a){
+            switch (sampleNow){
+                case 0:
+                    angle = Math.toRadians(120);
+                    break;
+                case 1:
+                    angle = Math.toRadians(90);
+                    break;
+                case 2:
+                    angle = Math.toRadians(60);
+                    break;
             }
-
-            data.addStringData("Test", "Data is coming");
-
-            RobotLiveSendTemp.send(data, "http://192.168.0.116");
+            robot.drive.mecanum.setMecanum(angle,0.3, 0, 1);
+        } else {
+            robot.drive.mecanum.updateMecanum(gamepad1,1);
+            sampleNow = sampPos;
         }
+//        if(data == null){
+//            telemetry.addData("Error","data is null");
+//
+//        }else {
+//            try {
+//                if (bm != null) {
+//                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//                    bm.compress(Bitmap.CompressFormat.JPEG, 2 /*ignored for PNG*/, bos);
+//
+//                    bos.close();
+//
+//                    byte[] bitmapdata = bos.toByteArray();
+//                    ByteArrayInputStream bs = new ByteArrayInputStream(bitmapdata);
+//
+//                    telemetry.addData("Image", bs != null);
+//                    telemetry.addData("Image2", bos != null);
+//
+//                    data.addLiveImage(bs);
+//                } else telemetry.addData("Error", "image is null");
+//
+//                data.addStringData("Test", "Data is coming");
+//
+//                RobotLiveSendTemp.send(data, "http://192.168.0.116");
+//            }catch(Exception e){
+//                telemetry.addData("Error",e);
+//            }
+//        }
     }
 
 
     @Override
     public Bitmap onFrame(Bitmap bm) {
         this.bm = bm;
-        telemetry.addLine("Image");
-        return bm;
+        Mat input = new Mat();
+        Mat hsv = new Mat();
+        Bitmap bmp32 = bm.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(bmp32, input);
+        Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
+        Mat mask = new Mat();
+        Core.inRange(hsv, new Scalar(10 * 180 / 255, 100, 100), new Scalar(30 * 180 / 255, 255, 255), mask);
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        Imgproc.findContours(mask, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        double maxArea = 0;
+        List<MatOfPoint> biggest = new ArrayList<>();
+        int index = -1;
+        Iterator<MatOfPoint> each = contours.iterator();
+        while (each.hasNext()) {
+            MatOfPoint wrapper = each.next();
+            double area = Imgproc.contourArea(wrapper);
+            if (area > maxArea) {
+                maxArea = area;
+                List<MatOfPoint> current = new ArrayList<>();
+                current.add(wrapper);
+                biggest = current;
+            }
+        }
+
+        //Centroid setup
+        Moments mmnts = Imgproc.moments(mask, true);
+        double locationX = mmnts.get_m10() / mmnts.get_m00();
+        double posX = (input.width() / 2) - locationX;
+        double size = maxArea;
+
+        telemetry.addData("Location", locationX);
+        telemetry.addData("Position", posX);
+        telemetry.addData("Size", size);
+
+        if(posX < -100){
+            sampPos = 2;
+        }else if(posX > -100 && posX < 100){
+            sampPos = 1;
+        }else if(posX > 100){
+            sampPos = 0;
+        }
+
+        telemetry.addData("Sample", sampPos);
+        return null;//PSVisionUtils.matToBitmap(hsv);
+//        return bm;
     }
 }
