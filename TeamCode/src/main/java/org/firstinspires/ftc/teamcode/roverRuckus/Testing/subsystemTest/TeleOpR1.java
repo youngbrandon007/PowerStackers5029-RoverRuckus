c4088495f2280d382fc50ed3457a2070f4c95734
Brandon Young <robotgenis@gmail.com>
2018-10-17 18:44:10 -0400

    v1.3.1
    Started PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/Testing/subsystemTest/TeleOpR1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/Testing/subsystemTest/TeleOpR1.java
@@ -2,15 +2,21 @@ package org.firstinspires.ftc.teamcode.roverRuckus.Testing.subsystemTest;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
 @TeleOp(name = "Subsystem", group = "test")
 public class TeleOpR1 extends config{
     @Override
     public void init() {
-
+        config(this);
     }
 
     @Override
     public void loop() {
-
+        testMotor();
+        telemetry.addData("GAMEPAD.X",gamepad1.left_stick_x);
+        telemetry.addData("GAMEPAD.Y",gamepad1.left_stick_y);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/Testing/subsystemTest/config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/Testing/subsystemTest/config.java
@@ -1,22 +1,56 @@
 package org.firstinspires.ftc.teamcode.roverRuckus.Testing.subsystemTest;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
 
 import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.PSConfigOpMode;
+import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.PSEnum;
+import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.PSRobot;
+import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.drive.control.motion.PIDController;
 import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.hardware.PSMotor;
-import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.hardware.PSServo;
 
 abstract class config extends PSConfigOpMode{
+
+    Drive drive;
+
     @Override
     public void config(OpMode opMode) {
-
+        robot = new PSRobot(opMode);
+        drive = new Drive();
     }
 
-    class drive{
+    public void testMotor(){
+        drive.motorRF.setPower(1.0);
+    }
+
+    class Drive{
         PSMotor motorRF;
         PSMotor motorLF;
         PSMotor motorRB;
         PSMotor motorLB;
+        double angle;
+        public PIDCoefficients SPEEDPID = new PIDCoefficients(-0.5, 0, 0);
+        public PIDController pidController = new PIDController(SPEEDPID);
+
+        public Drive(){
+            motorRF = robot.motorHandler.newDriveMotor("RF", PSEnum.MotorLoc.RIGHTFRONT, 1);
+            motorLF = robot.motorHandler.newDriveMotor("LF", PSEnum.MotorLoc.RIGHTFRONT, 1);
+            motorRB = robot.motorHandler.newDriveMotor("RB", PSEnum.MotorLoc.RIGHTFRONT, 1);
+            motorLB = robot.motorHandler.newDriveMotor("LB", PSEnum.MotorLoc.RIGHTFRONT, 1);
+            pidController.setOutputBounds(-1,1);
+            pidController.reset();
+        }
+
+        public void updateSpeed(double angle, double speed){
+            pidController.setSetpoint(speed);
+
+        }
+        public void update(){
+            robot.drive.mecanum.setMecanum(angle,pidController.getError(getSpeed()));
+        }
+
+        public double getSpeed() {
+
+        }
     }
 }
