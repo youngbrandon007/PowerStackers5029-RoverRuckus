ad0b1aa017e030043900dae4f10631bf2fa4f07a
Brandon Young <robotgenis@gmail.com>
2018-11-02 19:29:03 -0400

    no message

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/Robot_r2/Config.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.roverRuckus.Robot_v2;
+
+import org.firstinspires.ftc.teamcode.PSRobotLibs.lib.PSConfigOpMode;
+
+class config extends PSConfigOpMode {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/Robot_r2/EncoderTesting.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.roverRuckus.Robot_r2;
+
+public class EncoderTesting {
+}
