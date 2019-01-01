38e0631d12f031bcf2a1697acc00a6b466b530dd
Brandon Young <robotgenis@gmail.com>
2019-01-01 16:33:59 -0500

    v2.3.3
    Test program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/Robot_r4/Line_r4.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.roverRuckus.Robot_r4;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "t.line", group = "r3")
+public class Line_r4 extends Config_r4 {
+
+
+    ElapsedTime time = new ElapsedTime();
+    private double cal = 0;
+
+    private Pose2d estimatedPose = new Pose2d(0, 0,0);
+
+    @Override
+    public void init() {
+        config(this);
+
+    }
+
+    @Override
+    public void init_loop() {
+
+        telemetry.addData("gyro.heading", gyro.getHeading());
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+        Trajectory trajectory = drive.trajectoryBuilder()
+                .lineTo(new Vector2d(24, 0))
+                .build();
+
+        drive.followTrajectory(trajectory);
+        time.reset();
+    }
+
+    @Override
+    public void loop() {
+        if (drive.isFollowingTrajectory()) {
+            telemetry.addData("error",drive.getFollowingError());
+            drive.update();
+        } else {
+            robot.drive.mecanum.updateMecanumThirdPerson(gamepad1, 1.0, Math.toRadians(gyro.getHeading() - cal));
+        }
+        estimatedPose = drive.getEstimatedPose();
+        telemetry.addData("pose", estimatedPose);
+        telemetry.addData("gyro", Math.toDegrees(estimatedPose.getHeading()));
+//        telemetry.addData("drive.pos", drive.getWheelPositions());
+        telemetry.update();
+    }
+
+}
